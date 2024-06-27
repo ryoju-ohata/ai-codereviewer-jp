34fd2f23d12d107258f6bdb9352bfa5a9e6b03a5
Ryoju Ohata <ryozu256@gmail.com>
2024-06-27 09:49:29 +0900

    fix: Fix prompt message

--- src/main.ts
+++ src/main.ts
@@ -86,6 +86,7 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
+- Write in Japanese.
 
 Review the following code diff in the file "${
     file.to
